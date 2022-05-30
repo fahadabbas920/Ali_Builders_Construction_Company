c2ed3173e8856ba29d137b196a6a441b04124d11
Fahad Abbas <95125159+fahadabbas920@users.noreply.github.com>
2022-05-30 23:22:01 +0400

    Add files via upload
    First Upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector('.menu-btn');
+const navLinks = document.querySelector('.nav-links');
+
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('active');
+    navLinks.classList.toggle('show');
+})
